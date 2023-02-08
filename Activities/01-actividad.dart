670156c2cff678188c5eb497cd4152f82afd21f8
bakkym <jcamilito6bh2002@gmail.com>
2023-02-08 15:00:37 -0500

    finished activity 01

--- Activities/01-actividad.dart
+++ Activities/01-actividad.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
-
 void main(List<String> args) {
-
   bool continuar = true;
   var datos = [];
 
-   while (continuar) {
+  while (continuar) {
     print('Ingrese sexo: 1.Hombre 2.Mujer');
     String sexo = stdin.readLineSync().toString();
     print('Ingrese Estado: 1.Soltero 2.Casado 3.Viudo');
@@ -14,138 +12,145 @@ void main(List<String> args) {
     print('Ingrese su edad');
     int edad = int.parse(stdin.readLineSync().toString());
 
-    
-      switch(sexo){
+    switch (sexo) {
+      case "1":
+        {
+          sexo = 'Hombre';
+        }
+        break;
 
-        case "1" : { sexo = 'Hombre';}
-          break;
+      case "2":
+        {
+          sexo = 'Mujer';
+        }
+        break;
+    }
 
-        case "2" : { sexo = 'Mujer';}
-          break;
+    switch (estado) {
+      case "1":
+        {
+          estado = 'Soltero';
+        }
+        break;
+      case "2":
+        {
+          estado = 'Casado';
+        }
+        break;
+      case "3":
+        {
+          estado = 'Viudo';
+        }
+        break;
+    }
 
-      }
+    datos.add({'sexo': sexo, 'estado': estado, 'edad': edad});
+    print(datos);
 
-      switch (estado) {
-        case "1": { estado = 'Soltero';}
-          break;
-        case "2": { estado = 'Casado';}
-          break;
-        case "3": { estado = 'Viudo';}
-          break;
-      }
-      
-      datos.add({'sexo':sexo,'estado':estado, 'edad':edad});
-      print(datos);
-      
-      print('desea continuar? 1.Sí 2.No');
-      String continuacion = stdin.readLineSync().toString();
+    print('desea continuar? 1.Sí 2.No');
+    String continuacion = stdin.readLineSync().toString();
 
-      switch (continuacion) {
-        case '1': { continuar = true;}
-          break;
-        
-        case '2': {continuar = false;}
-          break;
-      }
+    switch (continuacion) {
+      case '1':
+        {
+          continuar = true;
+        }
+        break;
 
-      
+      case '2':
+        {
+          continuar = false;
+        }
+        break;
+    }
+  }
 
-      }
-
-      print("""1.Porcentaje hombres solteros
+  bool resultados = true;
+  while (resultados) {
+    print("""1.Porcentaje hombres solteros
       2.Edad promedio hombres casados
       3. Porcentaje mujeres solteras
+      4. Salir
       """);
 
-      String menu = stdin.readLineSync().toString();
+    String menu = stdin.readLineSync().toString();
 
-
-      switch (menu) {
-        case '1': {
+    switch (menu) {
+      case '1':
+        {
           dynamic contadorHombresSolteros = 0;
           dynamic contadorHombres = 0;
           for (var element in datos) {
             String genero = element['sexo'];
             String estadoCivil = element['estado'];
-            
 
             if (genero == 'Hombre' && estadoCivil == 'Soltero') {
-              contadorHombresSolteros ++;
+              contadorHombresSolteros++;
             }
 
-            if (genero == 'Hombre'){
-              contadorHombres ++;
-
+            if (genero == 'Hombre') {
+              contadorHombres++;
             }
-
-            
           }
-          dynamic porcentajeHombresSolteros = contadorHombresSolteros * 100 / contadorHombres;
-          print('el porcentaje de hombres solteros es: $porcentajeHombresSolteros');
-
-
-     }
-          break;
-
-        case '2': {
-
-          var edadesHombresCasados = [];
-          int sum = 0;
-
-            for (var element in datos) {
-            String genero = element['sexo'];
-            String estadoCivil = element['estado'];
-            
-
-            if (genero == 'Hombre' && estadoCivil == 'Casado') {
-              edadesHombresCasados.add(element['edad']);
-            }
-
-
-       
+          dynamic porcentajeHombresSolteros =
+              contadorHombresSolteros * 100 / contadorHombres;
+          print(
+              'el porcentaje de hombres solteros es: $porcentajeHombresSolteros');
         }
-            for (var i = 0; i < edadesHombresCasados.length; i++) {
-              sum += i;
-            }
-
-            var promedioEdades = sum / edadesHombresCasados.length;
-            print('El promedio de edad de los hombres casados es: $promedioEdades');
-
-      
-      }
         break;
 
+      case '2':
+        {
+          int sumEdades = 0;
+          int count = 0;
 
-        case '3': {
-              dynamic contadorMujeresSolteras = 0;
-              dynamic contadorMujeres = 0;
-              for (var element in datos) {
-                String genero = element['sexo'];
-                String estadoCivil = element['estado'];
-                
+          for (var element in datos) {
+            String genero = element['sexo'];
+            String estadoCivil = element['estado'];
+            int edad = element['edad'];
 
-                if (genero == 'Mujer' && estadoCivil == 'Soltero') {
-                  contadorMujeresSolteras ++;
-                }
+            if (genero == 'Hombre' && estadoCivil == 'Casado') {
+              sumEdades += edad;
+              count ++;
 
-                if (genero == 'Mujer'){
-                  contadorMujeres++;
+            }
+          }
 
-                }
 
-                
-              }
-              dynamic porcentajeMujeresSolteras = contadorMujeresSolteras * 100 / contadorMujeres;
-              print('El porcentaje de mujeres solteras es: $porcentajeMujeresSolteras');
-          
-     }
-       break;
-  
+          var promedioEdades = sumEdades / count;
+          print(
+              'El promedio de edad de los hombres casados es: $promedioEdades');
+        }
+        break;
 
-   
+      case '3':
+        {
+          dynamic contadorMujeresSolteras = 0;
+          dynamic contadorSolteros = 0;
+          for (var element in datos) {
+            String genero = element['sexo'];
+            String estadoCivil = element['estado'];
 
-   
-  
-}
+            if (genero == 'Mujer' && estadoCivil == 'Soltero') {
+              contadorMujeresSolteras++;
+            }
 
+            if (estadoCivil == 'Soltero') {
+              contadorSolteros++;
+            }
+          }
+          dynamic porcentajeMujeresSolteras =
+              contadorMujeresSolteras * 100 / contadorSolteros;
+          print(
+              'El porcentaje de mujeres solteras es: $porcentajeMujeresSolteras');
+        }
+        break;
+
+      case '4':
+        {
+          resultados = false;
+        }
+        break;
+    }
+  }
 }
